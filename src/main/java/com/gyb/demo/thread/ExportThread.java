0785697dbe5c9e1b9b73307f3fb82b5f7ec650d0
guoyubo <15776547151@163.com>
2021-10-04 00:30:08 +0800

    使用线程池处理

---
+++ src/main/java/com/gyb/demo/thread/ExportThread.java
@@ -0,0 +1,51 @@
+package com.gyb.demo.thread;
+
+import com.gyb.demo.bean.Student;
+import com.gyb.demo.service.StudentService;
+import com.gyb.demo.util.StudentExport;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.poi.ss.usermodel.Sheet;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author 郭煜博
+ * @version 1.0
+ * description:
+ * @date 2021/10/3 3:14
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+public class ExportThread implements Runnable {
+    private final CountDownLatch cdl;
+    private int half;
+    private int size;
+    private StudentService studentService;
+    private StudentExport studentExport;
+    private Sheet sheet;
+    private int index;
+    private int number;
+    private int start;
+
+
+
+    @Override
+    public void run() {
+        List<Student> studentList1;
+        for (int i = 1; i <= (half + size - 1) / size; i++) {
+            int y = (i - 1) * size + start;
+            studentList1 = studentService.findAllStudent(y, y + size - 1);
+            for (Student student : studentList1) {
+                studentExport.createEveryRow(sheet, student, index++, number++);
+            }
+            studentList1.clear();
+        }
+        cdl.countDown();
+
+
+    }
+}
--- src/main/java/com/gyb/demo/util/StudentExport.java
+++ src/main/java/com/gyb/demo/util/StudentExport.java
@@ -1,10 +1,9 @@
 package com.gyb.demo.util;
 
-import com.gyb.demo.DemoApplication;
 import com.gyb.demo.bean.Student;
 import com.gyb.demo.service.StudentService;
+import com.gyb.demo.thread.ExportThread;
 import org.apache.commons.io.IOUtils;
-import org.apache.poi.ss.formula.functions.T;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -16,8 +15,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.*;
 
 /**
  * @author gb
@@ -27,13 +25,17 @@ import java.util.concurrent.CountDownLatch;
  */
 @Component
 public class StudentExport extends BigExcelStyle {
-    private static int SIZE = 20000;
-
+    private static int SIZE = 30000;
+    final CountDownLatch cdl = new CountDownLatch(2);
+    @Autowired
+    private StudentExport studentExport;
     @Autowired
     private StudentService studentService;
-    final CountDownLatch cdl = new CountDownLatch(2);
 
     public void exportExcel(File file) throws InterruptedException, IOException {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor
+                (2, 4, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(4));
+
         int row = studentService.getRows();
         SXSSFWorkbook wb = new SXSSFWorkbook(60000);
         wb.setCompressTempFiles(true);
@@ -45,23 +47,19 @@ public class StudentExport extends BigExcelStyle {
         createHeadRow(wb, sheet);
         createHeadRow(wb, sheet2);
         int half = row / 2;
-        Thread t = new Thread(() -> {
-            List<Student> studentList1 = null;
-            int index = 1;
-            int number = 1;
-            for (int i = 1; i <= (half + SIZE - 1) / SIZE; i++) {
-                int y = (i - 1) * SIZE + 1;
-                studentList1 = studentService.findAllStudent(y, y + SIZE - 1);
-                for (Student student : studentList1) {
-                    createEveryRow(sheet2, student, index++, number++);
-                }
-                studentList1.clear();
-            }
-            cdl.countDown();
-        });
-        t.start();
+
+
         long l = System.currentTimeMillis();
-        Thread t1 = new Thread(() -> {
+        ExportThread exportThread1 = new ExportThread(cdl, half, SIZE, studentService, studentExport, sheet, 1, 1, 1);
+        Future<?> submit = executor.submit(exportThread1);
+        System.out.println("第一个" + submit.isDone());
+        ExportThread exportThread2 = new ExportThread(cdl, half, SIZE, studentService, studentExport, sheet2, 1, 1, 150001);
+        Future<?> submit1 = executor.submit(exportThread2);
+
+
+
+
+     /*   Thread t1 = new Thread(() -> {
             int index = 150001;
             int number = 150001;
             List<Student> studentList2 = null;
@@ -76,7 +74,7 @@ public class StudentExport extends BigExcelStyle {
             cdl.countDown();
         });
 
-        t1.start();
+        t1.start();*/
 
         try {
             cdl.await();
@@ -111,7 +109,7 @@ public class StudentExport extends BigExcelStyle {
      * @param number
      * @return
      */
-    private void createEveryRow(Sheet sheet, Student student, int i, int number) {
+    public void createEveryRow(Sheet sheet, Student student, int i, int number) {
         Row row1 = sheet.createRow(i);
         int line = 0;
         Cell cell1 = row1.createCell(line++);
